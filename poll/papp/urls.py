80310bd8a5c8f4fae0971fdc642c78cbb43c6448
Olaposi <seunposifunmi@gmail.com>
2022-04-20 15:14:44 +0100

    removed project

--- poll/papp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path('', views.hello),
-    path('vote', views.vote),
-    path('details/<int:question_id>', views.details)
-    
-]
